c84ab03fbee200775c3c65c7b00451ca598a06a3
Matthew <matthew930823@gmail.com>
2024-09-28 15:19:58 +0800

    Initial Commit

---
+++ demo1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  demo1
+//
+//  Created by user11 on 2024/9/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ demo1/demo1App.swift
@@ -0,0 +1,17 @@
+//
+//  demo1App.swift
+//  demo1
+//
+//  Created by user11 on 2024/9/28.
+//
+
+import SwiftUI
+
+@main
+struct demo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
